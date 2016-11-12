164feb0a7dc8d81cb73511e57b7918de59af08f0
gopa5979 <agopar@csumb.edu>
2016-11-12 22:05:57 +0000

    Added mock data to login and student information

---
+++ src/main/sql/studentData.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO Login (Username,Name,LastName, Password) VALUES ( "Sal", "Salvador","Ramirez","123");
+INSERT INTO Login (Username,Name,LastName, Password) VALUES ( "Jozy", "Jose","Cortez","123");
+                       
+                       
+INSERT INTO StudentInfo(name, lastName, grade, school, level, custodyIssueDescription, custodyIssue, needCurrentApplication)
+            VALUES("Salvador", "Ramirez", "Senior", "Alisal",2, "Dad has custody", 1, 0);
+INSERT INTO StudentInfo(name, lastName, grade, school, level, custodyIssueDescription, custodyIssue, needCurrentApplication)
+            VALUES("Eli", "Morgan", "Junior", "Salinas High",3, "None", 0, 0);
+
+            
+      
+    
+                       
+            
